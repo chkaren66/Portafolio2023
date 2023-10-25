68a6633fa29a72206609c4b241a2071830cba266
KarenChoquecallata <chkaren29@gmail.com>
2023-10-25 16:11:09 -0400

    responsive menu

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,17 @@
 var nav = document.querySelectorAll("header nav > a");
 console.log(nav);
+var options = document.querySelector("nav");
+console.log(options);
+
+var iconNav = document.querySelector(".icon-menu");
+console.log(iconNav);
+iconNav.addEventListener('click', function(){
+    if(options.classList.contains("active")){
+        options.classList.add("inactive")
+        options.classList.remove("active")
+    }else{
+        options.classList.add("active")
+        options.classList.remove("inactive")
+    }
+  
+});
